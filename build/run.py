aee58c19fa23857ba090528d705d1ebc2881c37d
Roxie Linden <roxie@lindenlab.com>
2024-03-06 13:28:22 -0800

    show branches available for checkout

--- build/run.py
+++ build/run.py
@@ -317,6 +317,7 @@ def get_webrtc(source_dir, patch_dir, version, target,
                 cmd(['git', 'remote', 'set-head', 'origin', '-a'])
                 cmd(['git', 'checkout', '-f', 'origin/HEAD'])
             else:
+                cmd(['git', 'branch'])
                 cmd(['git', 'checkout', '-f', version])
             cmd(['git', 'clean', '-df'])
             cmd(['gclient', 'sync', '-D', '--force', '--reset', '--with_branch_heads'])
