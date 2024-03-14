bf270729594ca90ac2c9f43544e37871bbc12f6c
Roxie Linden <roxie@lindenlab.com>
2024-03-13 21:50:33 -0700

    Some more fixup

--- build-cmd.sh
+++ build-cmd.sh
@@ -2,7 +2,6 @@
 
 cd "$(dirname "$0")"
 
-platform_name="$1"
 # turn on verbose debugging output for logs.
 exec 4>&1; export BASH_XTRACEFD=4; set -x
 
@@ -19,15 +18,28 @@ set -u
 
 top="$(pwd)"
 stage="$top"/stage
-mkdir "$stage"
 
-autobuild="$AUTOBUILD"
+case "$AUTOBUILD_PLATFORM" in
+    windows64)
+        autobuild="$(cygpath -u "$AUTOBUILD")"
+        platform_name="windows_x86_64"
+    ;;
+    darwin64)
+        autobuild="$AUTOBUILD"
+        platform_name="macos_x86_64"
+    ;;
+    *)
+        echo "This project is not currently supported for $AUTOBUILD_PLATFORM" 1>&2 ; exit 1
+        
+    ;;
+esac
 
 source_environment_tempfile="$stage/source_environment.sh"
 "$autobuild" source_environment > "$source_environment_tempfile"
 . "$source_environment_tempfile"
 
-tar xjf --strip-components=1 "$top"/webrtc."${platform_name}".tar.bz2
+pushd "$stage"
+tar --strip-components=1 -xjf "$top"/webrtc."${platform_name}".tar.bz2
 
 # Munge the WebRTC Build package contents into something compatible
 # with the layout we use for other autobuild pacakges
@@ -37,10 +49,16 @@ mv webrtc include
 mv lib release
 mkdir lib
 mv release lib
-mv Frameworks/WebRTC.xcframework/"${platform_name}"/macos-x86_64/WebRTC.framework lib/release
 mkdir LICENSES
 mv NOTICE LICENSES/webrtc-license.txt
 
+case "$AUTOBUILD_PLATFORM" in
+    darwin64)
+        mv Frameworks/WebRTC.xcframework/macos-x86_64/WebRTC.framework lib/release
+    ;;
+esac
+
+popd
 version=${RELEASE#*=}
 build=${AUTOBUILD_BUILD_ID:=0}
 echo "${version}.${build}" > "${stage}/VERSION.txt"
