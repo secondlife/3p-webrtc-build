8ed5c28310874e436fef5c48523ebff3af58ce0f
Roxie Linden <roxie@lindenlab.com>
2024-03-16 18:29:45 -0700

    delete temporary artifact when we don't need it anymore

--- build-cmd.sh
+++ build-cmd.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd "$(dirname "$0")"
-asset_id="$1"
+artifact_id="$1"
 
 # turn on verbose debugging output for logs.
 exec 4>&1; export BASH_XTRACEFD=4; set -x
@@ -41,7 +41,19 @@ source_environment_tempfile="$stage/source_environment.sh"
 
 pushd "$stage"
 
-curl -o webrtc.tar.bz2.zip -L -H "Authorization: Bearer $AUTOBUILD_GITHUB_TOKEN" https://api.github.com/repos/secondlife/3p-webrtc-build/actions/artifacts/"$asset_id"/zip
+# download the artifact
+curl -o webrtc.tar.bz2.zip -L -H "Accept: application/vnd.github+json" \
+                              -H "X-GitHub-Api-Version: 2022-11-28" \
+                              -H "Authorization: Bearer $AUTOBUILD_GITHUB_TOKEN" \
+                              https://api.github.com/repos/secondlife/3p-webrtc-build/actions/artifacts/"$artifact_id"/zip
+
+# delete it so autobuild upload doesn't assume it's a bad autobuild artifact
+# TODO: Find a better way to do this that doesn't involve a 3rd party action
+curl -X DELETE -H "Accept: application/vnd.github+json" \
+               -H "X-GitHub-Api-Version: 2022-11-28" \
+               -H "Authorization: Bearer $AUTOBUILD_GITHUB_TOKEN" \
+               https://api.github.com/repos/secondlife/3p-webrtc-build/actions/artifacts/"$artifact_id"
+
 unzip webrtc.tar.bz2.zip
 tar xjf webrtc.tar.bz2 --strip-components=1
 rm webrtc.tar.bz2.zip
