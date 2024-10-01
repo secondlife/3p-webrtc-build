5723e428985b44e52952209a2442b4a7b5e1193f
Roxie Linden <roxie@lindenlab.com>
2024-10-01 15:06:52 -0700

    Make it build locally on latest xcode/sonoma

--- build/run.py
+++ build/run.py
@@ -226,6 +226,7 @@ PATCHES = {
         'disable_mute_of_audio_processing.patch',
         'crash_on_fatal_error.patch',
         'dont_fatal_error_on_audio_device_stop.patch',
+        'Revert-mac-Work-around-an-inccorect-availability-ann.patch',
     ],
     'macos_arm64': [
         'add_license_dav1d.patch',
@@ -236,6 +237,7 @@ PATCHES = {
         'disable_mute_of_audio_processing.patch',
         'crash_on_fatal_error.patch',
         'dont_fatal_error_on_audio_device_stop.patch',
+        'Revert-mac-Work-around-an-inccorect-availability-ann.patch',
     ],
     'ios': [
         'add_license_dav1d.patch',
